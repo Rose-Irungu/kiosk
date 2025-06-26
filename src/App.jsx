6c828b78ea7ca20b19e2c7e7189e763b9e611c8d
Rose Irungu <127296273+Rose-Irungu@users.noreply.github.com>
2025-06-26 13:10:39 +0300

    updated some files

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
-import VisitorPage from './pages/VisitorPage'; // ✅ Import the Visitor Page
+import VisitorPage from './pages/VisitorPage';
+import Verify from './pages/Verify';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<VisitorPage />} /> {/* ✅ Set VisitorPage as default */}
+        {/* <Route path="/" element={<Welcome />} /> {/* brian Uncomment when done*/}
+        <Route path="/" element={<VisitorPage />} /> 
         <Route path="/header" element={<Header />} />
         <Route path="/navbar" element={<Navbar />} />
+        <Route path="/verify" element={<Verify />} />
+        {/* <Route path="/welcomeback" element={< Welcomeback/>} /> */}{/* Nigel uncomment when done */}
+        {/* <Route path="/checkout" element={<checkout />} /> */}{/* Nigel uncomment when done */}
+        {/* <Route path="/welcomeback" element={<WelcomeBack />} /> */}{/* Nigel uncomment when done */}
+        {/* <Route path="/bye" element={<Bye />} /> */}{/* Rebecca uncomment when done */}
+        {/* <Route path="/error" element={<Error />} /> */}{/* Rebecca uncomment when done */}
+       
+
+        
       </Routes>
     </Router>
   );
