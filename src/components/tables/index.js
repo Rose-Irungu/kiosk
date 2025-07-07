863212cd1300f34029c3d42c09b454c0d7d07326
Rose Irungu <127296273+Rose-Irungu@users.noreply.github.com>
2025-07-07 12:53:06 +0300

    added the skeleton ui

---
+++ src/components/tables/index.js
@@ -0,0 +1,5 @@
+export { default as DashboardTable } from './DashboardTable';
+export { default as Users } from './Users';
+export { default as Visitors } from './Visitors';
+export { default as Emergencies } from './Emergencies';
+export { default as Incident } from './Incident';
