387920c0f33e76aefbad357a7920267f487bdb92
dotie-the-programmer <dorothycherono951@gmail.com>
2025-07-08 15:19:21 +0300

    fix: import userform component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { UserForm } from './components/forms/form';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Navigation from './components/Navigation';
