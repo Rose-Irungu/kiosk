d8d48d4ff369130445b54dd3e3781ff02c93efe8
Rose Irungu <127296273+Rose-Irungu@users.noreply.github.com>
2025-07-09 16:32:16 +0300

    refactor: comment out i18n setup code and update Vite config for esbuild target

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,10 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+
     },
+      esbuild: {
+    target: 'esnext' // or 'es2022'
+  }
   },
 })
