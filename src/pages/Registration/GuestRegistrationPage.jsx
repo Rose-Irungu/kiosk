c5d29a3182226eee313a4cdbe1f7a6e1a3776ed3
dotie-the-programmer <dorothycherono951@gmail.com>
2025-07-10 15:09:57 +0300

    form name changed

--- src/pages/Registration/GuestRegistrationPage.jsx
+++ src/pages/Registration/GuestRegistrationPage.jsx
@@ -3,24 +3,32 @@ import logo from "../../assets/logo.svg";
 import rectangle from "../../assets/rectangle-780.png";
 // import sphere from "../assets/sphere-green-glossy0.png";
 import Header from "../../components/Header";
-import {GuestFormFields} from "../../components/forms/GuestFormFields";
+import RegistrationForm from "../../components/forms/GuestCheckInForm";
+// import { GuestFormFields } from "../../components/forms/GuestFormFields";
+
 function GuestRegistrationPage() {
   return (
     <div className="flex min-h-screen w-full flex-col overflow-x-hidden bg-white">
       <Header />
       <main className="flex flex-1 flex-col lg:flex-row">
         <section
-            className="relative flex h-48 w-full flex-shrink-0 items-center justify-center bg-cover bg-center lg:h-auto lg:w-1/2"
-            style={{ backgroundImage: `url(${rectangle})` }}>
-
-            <img src={logo} alt="West Brook Logo" className="w-32 md:w-44 lg:w-60 xl:w-72" />
+          className="relative flex h-48 w-full flex-shrink-0 items-center justify-center bg-cover bg-center lg:h-auto lg:w-1/2"
+          style={{ backgroundImage: `url(${rectangle})` }}
+        >
+          <img
+            src={logo}
+            alt="West Brook Logo"
+            className="w-32 md:w-44 lg:w-60 xl:w-72"
+          />
         </section>
+
         <section className="relative flex w-full flex-col items-center bg-[#E6FBE9] px-4 pb-12 pt-4 lg:w-1/2 lg:px-10">
-        <GuestFormFields />
+          {/* <GuestFormFields /> */}
+          <RegistrationForm />
         </section>
       </main>
     </div>
-  )
+  );
 }
 
 export default GuestRegistrationPage;
