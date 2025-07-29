80d758ca8ebc49c35c2eb431d87119a3f672da21
Nigel Khasiani <nigelkhasiani@gmail.com>
2025-07-29 13:11:07 +0300

    Intergrated components into the bottom div of the dashboard

--- src/pages/ResidentPages/ResidentDashboard.jsx
+++ src/pages/ResidentPages/ResidentDashboard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 //import ResidentLayout from '../../components/ResidentComponents/ResidentLayout';
-import InviteGuest from '../../components/ResidentComponents/Buttons/InviteGuest';
+//import InviteGuest from '../../components/ResidentComponents/Buttons/InviteGuest';
 import SosButton from '../../components/ResidentComponents/Buttons/SosButton';
 import SafetyProtocolsButton from '../../components/ResidentComponents/Buttons/SafetyProtocolsButton';
-import PastGuestsButton from '../../components/ResidentComponents/Buttons/PastGuestsButton';
+//import PastGuestsButton from '../../components/ResidentComponents/Buttons/PastGuestsButton';
 
 //Service imports
 import {inviteGuest, triggerSOS, clickTest} from '../../services/residentDashboardServices';
@@ -11,10 +11,18 @@ import {inviteGuest, triggerSOS, clickTest} from '../../services/residentDashboa
 function ResidentDashboard() {
   return (
      <div className="flex flex-wrap justify-start mb-[12px]">
-        <InviteGuest callback={inviteGuest}/>
-        <SosButton callback = {triggerSOS}/>
-        <SafetyProtocolsButton />
-        <PastGuestsButton label="Today" callback={clickTest}/>
+        <div className='flex flex-col w-full lg:h-[165px] top-[784px] rounded-[12px] py-[10px] px-[20px] bg-[#FDE8E7]'>
+            <div className='flex flex-row justify-between w-full h-[32px]'>
+                <div className='flex flex-row gap-[12px] w-[262px] h-[32px]'>
+                    <div className='h-[32px] w-[32px]'></div>
+                    <h1 className='font-dmsans font-semibold text-[24px] text-[#610C07]'>Emergencies Panel</h1>
+                </div>
+            </div>
+            <div className='flex flex-row justify-between w-full h-[96px] rounded-[12px] py-[12px] px-[14px]'>
+                <SosButton callback={triggerSOS}/>
+                <SafetyProtocolsButton callback={clickTest}/>
+            </div>
+        </div>
      </div>
   );
 }
