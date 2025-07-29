7e5d5a9fe2ba8cb61e452abd8482cde7c0bee309
Nigel Khasiani <nigelkhasiani@gmail.com>
2025-07-29 15:17:05 +0300

    worked o the resident dashboard page

--- src/pages/ResidentPages/ResidentDashboard.jsx
+++ src/pages/ResidentPages/ResidentDashboard.jsx
@@ -3,22 +3,35 @@ import React from 'react';
 //import InviteGuest from '../../components/ResidentComponents/Buttons/InviteGuest';
 import SosButton from '../../components/ResidentComponents/Buttons/SosButton';
 import SafetyProtocolsButton from '../../components/ResidentComponents/Buttons/SafetyProtocolsButton';
-//import PastGuestsButton from '../../components/ResidentComponents/Buttons/PastGuestsButton';
+import PastGuestsButton from '../../components/ResidentComponents/Buttons/PastGuestsButton';
 
 //Service imports
 import {inviteGuest, triggerSOS, clickTest} from '../../services/residentDashboardServices';
 
 function ResidentDashboard() {
   return (
-     <div className="flex flex-wrap justify-start mb-[12px]">
-        <div className='flex flex-col w-full lg:h-[165px] top-[784px] rounded-[12px] py-[10px] px-[20px] bg-[#FDE8E7]'>
+     <div className="flex flex-wrap justify-start mb-[12px] space-y-4">
+        <div className='flex flex-col w-full lg:h-[222px] top-[521px] rounded-[12px] py-[8px] px-[12px] bg-[#F0EEFD] gap-[10px]'>
+            <div className='flex flex-row justify-between w-full h-[32px]'>
+                <div className='flex flex-row w-[182px] h-[32px]'>
+                    <div className='h-[32px] w-[32px]'></div>
+                    <h1 className='font-dmsans font-semibold text-[24px] text-[#002706]'>Past Guests</h1>
+                </div>
+            </div>
+            <div className='flex flex-row justify-between w-full h-[40px] rounded-[24px] py-[4px] px-[8px] gap-[10px]'>
+                <PastGuestsButton label={"Today"} callback={() => clickTest}/>
+                <PastGuestsButton label={"This Week"} callback={() => clickTest} />
+                <PastGuestsButton label={"This Month"} callback={() => clickTest} />
+            </div>
+        </div>
+        <div className='flex flex-col w-full lg:h-[165px] lg:top-[784px] rounded-[12px] py-[10px] px-[20px] bg-[#FDE8E7]'>
             <div className='flex flex-row justify-between w-full h-[32px]'>
                 <div className='flex flex-row gap-[12px] w-[262px] h-[32px]'>
                     <div className='h-[32px] w-[32px]'></div>
                     <h1 className='font-dmsans font-semibold text-[24px] text-[#610C07]'>Emergencies Panel</h1>
                 </div>
             </div>
-            <div className='flex flex-row justify-between w-full h-[96px] rounded-[12px] py-[12px] px-[14px]'>
+            <div className='flex flex-row justify-between w-full h-[96px] rounded-[12px] py-[12px] px-[14px] space-x-4'>
                 <SosButton callback={triggerSOS}/>
                 <SafetyProtocolsButton callback={clickTest}/>
             </div>
