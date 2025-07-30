347c92a329a13778be1f4fb46fc6a2828fcc3748
Nigel Khasiani <nigelkhasiani@gmail.com>
2025-07-30 17:31:49 +0300

    first attempt of adding Emeregency contact card

--- src/components/ResidentComponents/SafetyCards/EmergencyContacts.jsx
+++ src/components/ResidentComponents/SafetyCards/EmergencyContacts.jsx
@@ -10,7 +10,7 @@ export default function EmergencyContacts({icon1, icon2,heading, phoneOne, phone
                 <img src={icon2} className='w-[24px] h-[24px] object-contain' />
                 <p className='font-dmsans font-normal text-[20px] leading-[100%] tracking-[0] text-[#002706]'>{phoneOne}</p>
             </div>
-            <div className='flex flex-row w-[181px] h-[40px] rounded-[8px] border-[1px] border-[#005E0E] p-[4px] gap-[4px]'>
+            <div className='flex flex-row text-center align-items-center justify-center w-[181px] h-[40px] rounded-[8px] border-[1px] border-[#005E0E] p-[4px] gap-[4px]'>
                 <img src={icon2} className='w-[24px] h-[24px] object-contain' />
                 <p className='font-dmsans font-normal text-[20px] leading-[100%] tracking-[0] text-[#002706]'>{phoneTwo}</p>
             </div>
