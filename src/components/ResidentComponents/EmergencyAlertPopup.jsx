79bc24ed2557a144540aed8393c92dcc87304652
Rose Irungu <127296273+Rose-Irungu@users.noreply.github.com>
2025-07-30 18:21:40 +0300

    feat: implement EmergencyAlertCard with confirmation buttons and SVG icons

--- src/components/ResidentComponents/EmergencyAlertPopup.jsx
+++ src/components/ResidentComponents/EmergencyAlertPopup.jsx
@@ -1,32 +1,56 @@
-import { AlertTriangle, Shield } from "lucide-react";
-
+import { useState } from "react";
 
 export default function EmergencyAlertCard() {
+
+   const [visible, setVisible] = useState(true);
+
+  const handleYes = () => {
+    console.log("✅ Alert confirmed!");
+    
+    setVisible(false); 
+  };
+
+  const handleNo = () => {
+    console.log("❌ Alert canceled.");
+    setVisible(false); 
+  };
+
+  if (!visible) return null;
   return (
-  
-    <div className="relative bg-red-50 rounded-3xl h-80 w-80 shadow-lg overflow-hidden">
-   
-      <div className="absolute  transform ">
-        <div className="relative   h-30 w-50">
-          <img src="/alert.svg" alt="alert icon" className="justify-center ml-[60px]"/>
-        
+    <div className="fixed top-[250px] left-1/2 transform -translate-x-1/2 bg-[#fde8e7] rounded-2xl w-[560px] h-auto shadow-lg overflow-hidden z-50 p-6">
+      <div className="flex gap-6 items-start">
+        <img src="/alert.svg" alt="alert icon" className="w-24 h-24" />
+
+        <div className="flex flex-col justify-between">
+          <p className="text-red-700 font-semibold text-lg mb-4">
+            Are you sure you want to send this alert?
+          </p>
+
+          <div className="flex gap-4">
+            <button 
+             onClick={handleYes}
+            className="flex items-center gap-2 px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition">
+              <img
+                src="/mdi-tick-circle0.svg"
+                alt="Yes Icon"
+                className="w-4 h-4"
+              />
+              <span>Yes</span>
+            </button>
+
+            <button 
+             onClick={handleNo}
+            className="flex items-center gap-2 px-4 py-2 border border-red-600 text-red-600 rounded-lg hover:bg-red-200 transition">
+              <img
+                src="/ic-round-cancel0.svg"
+                alt="No Icon"
+                className="w-4 h-4"
+              />
+              <span>No</span>
+            </button>
+          </div>
         </div>
       </div>
-      
-   
-      <div className="absolute left-1/2 top-48 transform -translate-x-1/2 w-60">
-        <p className="text-red-500 text-center font-medium text-xl">
-          Emergency Alert Sent
-        </p>
-      </div>
-      
-  
-      <div className="absolute left-1/2 top-56 transform -translate-x-1/2 w-72">
-        <p className="text-red-500 text-center font-medium text-sm">
-          Security guards are on the way
-        </p>
-      </div>
     </div>
-   
   );
 }
