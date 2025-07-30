2020bc1653d63ebf2de366f3b1f5fc990333e60a
Nigel Khasiani <nigelkhasiani@gmail.com>
2025-07-30 11:42:32 +0300

    defined the page sections for the Safety protocols page

--- src/pages/ResidentPages/SafetyProtocols.jsx
+++ src/pages/ResidentPages/SafetyProtocols.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function SafetyProtocols() {
   return (
     <div className="flex flex-wrap justify-start mb-[12px] space-y-4">
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className='flex flex-row w-full lg:h-[376px] rounded-[12px] p-[20px] gap-[46px] bg-[#FFFFFF]'></div>
+        <div className='flex flex-row w-full h-[102px] rounded-[12px] p-[10px] gap-[10px] bg-[#E6FBE9]'></div>
+        <div className='flex flex-col w-full h-[432px] rounded-[12px] p-[20px] gap-[12px] bg-[#FFFFFF]'></div>
     </div>
   );
 }
