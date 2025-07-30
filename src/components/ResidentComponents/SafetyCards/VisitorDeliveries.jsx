96521e61b997e1103c14f5b4deb6ac28753cb12e
Nigel Khasiani <nigelkhasiani@gmail.com>
2025-07-30 15:20:50 +0300

    added section heading to all the Safety Cards

--- src/components/ResidentComponents/SafetyCards/VisitorDeliveries.jsx
+++ src/components/ResidentComponents/SafetyCards/VisitorDeliveries.jsx
@@ -5,7 +5,9 @@ export default function VisitorDeliveries() {
   return (
     <div className='flex flex-col w-[728px] md:w-full sm:w-full h-[256.8px] gap-[21px]'>
       <Head/>
-      <div className='flex flex-col lg:w-[728px] lg:h-[175.8] gap-[7px]'></div>
+      <div className='flex flex-col lg:w-[728px] lg:h-[175.8] gap-[7px]'>
+        <SectionHeading icon="" text = ""/>
+      </div>
     </div>
   );
 }
