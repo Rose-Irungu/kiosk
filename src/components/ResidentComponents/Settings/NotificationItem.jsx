d32c22b86ca410ba8500189d62da8a3411ecdf96
Nigel Khasiani <nigelkhasiani@gmail.com>
2025-08-05 17:19:01 +0300

    almost there

---
+++ src/components/ResidentComponents/Settings/NotificationItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ToggleSwitch from './ToggleSwitch';
+
+export default function NotificationItem({icon, text, callback}) {
+  return (
+    <div className='flex flex-row justify-between w-full h-[48px] border-bottom-[1px] border-bottom-[#0000004D] py-[12px] px-[10px]'>
+        <div className='flex flex-row w-[226px] h-[24px] gap-[12px]'>
+            <div className='w-[24px] h-[24px] rounded-[20px]'>
+                <img src={icon} className='w-full h-full object-cover' />
+            </div>
+            <p className='font-dmsans font-medium text-[16px] text-[#002706] leading-[100%] tracking-[0]'>{text}</p>
+        </div>
+        <ToggleSwitch onToggle={()=>callback()}/>
+    </div>
+  );
+}
--- src/pages/ResidentPages/ResidentSettings.jsx
+++ src/pages/ResidentPages/ResidentSettings.jsx
@@ -5,6 +5,7 @@ import Header from '../../components/ResidentComponents/Settings/Header';
 import PersonalInformation from '../../components/ResidentComponents/Settings/PersonalInformation';
 import Cars from '../../components/ResidentComponents/Settings/Cars';
 import HouseholdInformation from '../../components/ResidentComponents/Settings/HouseholdInformation';
+import NotificationSettings from '../../components/ResidentComponents/Settings/NotificationSettings';
 
 export default function ResidentSettings() {
   return (
@@ -16,6 +17,7 @@ export default function ResidentSettings() {
           <Cars />
           <HouseholdInformation />
           <Header icon="/gear.svg" text={"My Settings"}/>
+          <NotificationSettings/>
         </div>
     </ResidentLayout>
   );
