5547e4b21172ec48e3b2b8b2bcda0111bccf7b8b
Nigel Khasiani <nigelkhasiani@gmail.com>
2025-08-05 15:44:59 +0300

    added personal information card

--- src/pages/ResidentPages/ResidentSettings.jsx
+++ src/pages/ResidentPages/ResidentSettings.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ResidentLayout from '../../components/ResidentComponents/ResidentLayout';
+import User from '../../components/ResidentComponents/Settings/User';
+import Header from '../../components/ResidentComponents/Settings/Header';
+import PersonalInformation from '../../components/ResidentComponents/Settings/PersonalInformation';
 
 export default function ResidentSettings() {
   return (
     <ResidentLayout>
-        <div className="flex flex-wrap justify-start mb-[12px] space-y-4"></div>
+        <div className="flex flex-wrap flex-col justify-start mb-[12px] space-y-4">
+          <User/>
+          <Header icon="/msee.svg" text={"My Profile"}/>
+          <PersonalInformation />
+        </div>
     </ResidentLayout>
   );
 }
