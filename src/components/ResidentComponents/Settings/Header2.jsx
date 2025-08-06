366092b54be67432066978d8e405a06358778a65
Nigel Khasiani <nigelkhasiani@gmail.com>
2025-08-06 11:55:59 +0300

    almost there

--- src/components/ResidentComponents/Settings/Header2.jsx
+++ src/components/ResidentComponents/Settings/Header2.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default function Header2({icon, text}) {
+export default function Header2({icon, text, callback}) {
   return (
     <div className='flex flex-row w-full h-32px gap-[12px]'>
         <div className='w-[24px] h-[24px]'>
-            <img src={icon} className='w-full h-full object-cover' />
+            <img src={icon} className='w-full h-full object-cover' 
+                            onClick={()=>callback()}
+            />
         </div>
         <div className='w-full h-full'>
             <h1 className='font-dmsans font-semibold text-[24px] mt-[4px] text-[#005E0E] leading-[100%] tracking-[0]'>{text}</h1>
--- src/pages/ResidentPages/LoggedDevices.jsx
+++ src/pages/ResidentPages/LoggedDevices.jsx
@@ -5,6 +5,7 @@ import Header2 from '../../components/ResidentComponents/Settings/Header2';
 import ResidentLayout from '../../components/ResidentComponents/ResidentLayout';
 import CurrentDevice from '../../components/ResidentComponents/Settings/CurrentDevice';
 import OtherDevice from '../../components/ResidentComponents/Settings/OtherDevice';
+import { useNavigate } from 'react-router-dom';
 
 //Sample test function
 function testButton(){
@@ -12,10 +13,11 @@ function testButton(){
 }
 
 export default function LoggedDevices() {
+    const navigate = useNavigate();
   return (
     <ResidentLayout>
         <div className="flex flex-wrap flex-col justify-start mb-[12px] space-y-4">
-            <Header2 icon="/mshale.svg" text="Logged In Devices"/>
+            <Header2 icon="/mshale.svg" text="Logged In Devices" callback={()=>navigate("/resident/settings")}/>
             <input type="text" className='flex flex-row w-full h-[40px] rounded-[8px] border-[1px] border-[#E6FBE9] gap-[10px] py-[3px] px-[16px] bg-[#FFFFFF] ' style={{marginTop:'16px'}} placeholder={"Search devices"}/>
             <CurrentDevice os="Android" place={"Nairobi, Kenya"} time={"4 hours ago"} browser={"chrome"}/>
             <OtherDevice os="Android" place={"Nairobi, Kenya"} time={"4 hours ago"} browser={"chrome"} callback={()=>testButton()}/>
