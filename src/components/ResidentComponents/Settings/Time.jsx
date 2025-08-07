e2d6d3574d7c42768f9fa33deafc96d0aacc69bb
Rose Irungu <127296273+Rose-Irungu@users.noreply.github.com>
2025-08-07 18:35:12 +0300

    fix

--- src/components/ResidentComponents/Settings/Time.jsx
+++ src/components/ResidentComponents/Settings/Time.jsx
@@ -3,7 +3,7 @@ import TimeIcon from './TimeIcon';
 
 export default function Time({time}) {
   return (
-    <div className='flex flex-row w-[120px] h-[20px] gap-[4px]'>
+    <div className='flex flex-row w-[122px] h-[20px] gap-[4px]'>
         <TimeIcon/>
         <div className='h-[16px] text-center'>
             <p className='font-dmsans font-medium text-[12px] mt-[4px] text-[#666666] leading-[100%] tracking-[0]'>
