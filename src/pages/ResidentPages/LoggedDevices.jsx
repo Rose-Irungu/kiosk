f08d8730acd46846a50b4e16a6a456f8eb439238
Nigel Khasiani <nigelkhasiani@gmail.com>
2025-08-07 14:57:12 +0300

    filtered current data

--- src/pages/ResidentPages/LoggedDevices.jsx
+++ src/pages/ResidentPages/LoggedDevices.jsx
@@ -49,7 +49,9 @@ export default function LoggedDevices() {
             <>
               <Header2 icon="/mshale.svg" text="Logged In Devices" callback={()=>navigate("/resident/settings")}/>
               <input type="text" className='flex flex-row w-full h-[40px] rounded-[8px] border-[1px] border-[#E6FBE9] gap-[10px] py-[3px] px-[16px] bg-[#FFFFFF] ' style={{marginTop:'16px'}} placeholder={"Search devices"}/>
-              <CurrentDevice os="Android" place={"Nairobi, Kenya"} time={"4 hours ago"} browser={"chrome"}/>
+              {current.length > 0 && current.map((device)=>{
+                return <CurrentDevice key={device.device_id} os={device.os} place={"Nairobi, Kenya"} time={"4 hours ago"} browser={device.browser}/>
+              })}
               {previous.length > 0 && previous.map((device) =>{
                 return <OtherDevice key={device.device_id} os={device.os} place={"Nairobi, Kenya"} time={"4 hours ago"} browser={device.browser} callback={()=>logoutDevice(device.device_id)}/>
               })}
