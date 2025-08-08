f662844dfe6b6b611d7ff4018a368860a64d793e
Rose Irungu <127296273+Rose-Irungu@users.noreply.github.com>
2025-08-08 15:14:40 +0300

    responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ import rectangle from "../assets/rectangle-780.png";
 const HomePage = () => {
   return (
     <div className="flex flex-col min-h-screen w-full max-w-full overflow-x-hidden relative">
-      
+      {/* Header */}
       <Header />
 
       {/* Content layout: responsive - vertical on mobile, horizontal on desktop */}
@@ -30,10 +30,7 @@ const HomePage = () => {
         </div>
 
         {/* Right form section */}
-     <div className="w-full lg:w-1/2 bg-[#E6FBE9] relative flex flex-col items-center px-4 sm:px-6 lg:px-8 py-6 lg:pt-0 lg:pb-10 min-h-screen">
-
-
-
+        <div className="w-full lg:w-1/2 bg-[#E6FBE9] relative flex flex-col items-center px-4 sm:px-6 lg:px-8 py-6 lg:pt-0 lg:pb-10 min-h-[500px] lg:min-h-full ">
           {/* Top bar with Navbar - only show on desktop */}
           <div className="hidden lg:flex w-full justify-between items-center mt-0 mb-10">
             <Navbar />
