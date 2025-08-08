a66aa6d8f7cb200d19dadb573d78d25e7bc5f5be
Rose Irungu <127296273+Rose-Irungu@users.noreply.github.com>
2025-08-08 14:53:20 +0300

    removed photo field

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Header = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
-  //const [language, setLanguage] = useState('EN');
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -12,41 +11,31 @@ const Header = () => {
     return () => clearInterval(timer);
   }, []);
 
-  const currentDate = currentTime.toLocaleDateString('en-US', {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
+  const currentDate = currentTime.toLocaleDateString("en-US", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   });
 
-  const timeString = currentTime.toLocaleTimeString('en-US', {
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: true
+  const timeString = currentTime.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
   });
-
- 
 
   return (
     <header className="flex justify-between items-center px-6 py-4 bg-[#00580d] relative z-50">
-     
       <div className=" text-white px-5 text-sm  text-[20px] ">
         {currentDate}
       </div>
-      
-     
+
       <div className="text-white  text-[24px]  uppercase text-center mr-45 flex-1 mx-5">
         VISITOR GATE
       </div>
-      
-      
+
       <div className="flex items-center gap-5 text-[20px]">
-        
-        <div className="text-white text-lg   min-w-fit">
-          {timeString}
-        </div>
-        
-       
+        <div className="text-white text-lg   min-w-fit">{timeString}</div>
       </div>
     </header>
   );
